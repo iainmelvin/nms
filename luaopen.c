22d527d99b91c9c6b97a2c4b3e20939abe1460a1
Iain Melvin <iain@nec-labs.com>
2019-09-04 17:31:25 -0400

    nmsapi

--- luaopen.c
+++ luaopen.c
@@ -1,6 +1,6 @@
 #include <lua.h>
 #ifdef _MSC_VER
 /* Allows building with MS compilers */
-int luaopen_libnmsapi(lua_State *L) { return 0; }
+int luaopen_nmsapi(lua_State *L) { return 0; }
 #endif 
 
